7437abf519d9ae3c12270f1d27a1ea76b1a898e4
Lisa Huang <lisa.yc.huang@gmail.com>
2018-01-17 21:54:09 -0800

    Complete User Signin and User signup tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   resources :attractions
+  root "application#welcome"
 
-  devise_for :users
+  get "/sign_up" => "users#new"
+  # get "/signin" => 
+
+  devise_for :users, path: '',
+    :path_names => { sign_in: 'signin', sign_up: 'users/new', sign_out: 'logout'},
+    :controllers => {:sessions => "sessions"}
+
 end
